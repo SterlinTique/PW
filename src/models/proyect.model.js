40ad4b100d5a302c9053a7882bb2487c118813b9
SterlinTique <jader.tique@utp.edu.co>
2025-03-10 21:26:52 -0500

    Corrigiendo archivo proyect.model.js

--- src/models/proyect.model.js
+++ src/models/proyect.model.js
@@ -5,7 +5,7 @@ const Proyect = sequelize.define('proyectos', {
     id: { type: DataTypes.INTEGER, primarykey: false, autoIncrement: true },
     nombre: { type: DataTypes.STRING, allowNull: false },
     descripcion: { type: DataTypes.STRING, allowNull: false }, 
-    fecha_creacion: { type: DataTypes.DATE, allowNull: false }, 
+    fecha_creacion: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW }, 
     administrador_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -15,6 +15,14 @@ const Proyect = sequelize.define('proyectos', {
 }, {
     timestamps: false,
     tableName: 'proyectos',
+    // Lo siguiente es para organizar la hora referente zona horaria de Colombia 
+    hooks: {
+        afterCreate: (proyect, options) => {
+            if (prpyect.fecha_creacion) {
+                proyect.fecha_creacion.setHours(proyect.fecha_creacion.getHours()- 5);// Se restan 5 horas para cuadrar la zona horaria
+            }
+        }
+    }
 });
 
 module.exports = Proyect;
