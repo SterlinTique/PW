14789880f8a8781643cda2a9cf76f47b06558925
SterlinTique <jader.tique@utp.edu.co>
2025-03-27 15:11:04 -0500

    Configurando /middlewares y /utils

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+    consulte.error(err.stack);
+    res.status(500).json({ message: 'Algo salió mal', error: err.message });
+};
+
+module.exports = errorHandler;
+
+
+
+
+// https:localhost:3000/api/v1/auth/login
+// como generar el token de postman usando javaScrip
+
